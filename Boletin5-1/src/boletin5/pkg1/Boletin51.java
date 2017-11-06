cf7e85ccf310a7620c44eec643aaedd08b7ce626
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-11-06 09:38:26 +0100

    Commit para git.

---
+++ Boletin5-1/src/boletin5/pkg1/Boletin51.java
@@ -0,0 +1,21 @@
+
+package boletin5.pkg1;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+import java.util.Scanner;
+public class Boletin51 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodos5_1 obxNum=new Metodos5_1();
+        Scanner sc=new Scanner (System.in);
+        obxNum.verNum(sc.nextInt());
+        
+    }
+    
+}
---
+++ Boletin5-1/src/boletin5/pkg1/Metodos5_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5.pkg1;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Metodos5_1 {
+    private int num;
+    
+    public void verNum(int num){
+        if (num > 0){
+            System.out.println("Es positivo");
+        }
+    }
+    
+    
+}
